efe95801a2fba47e9048ddc83ab365cf13c55bc9
piyushkarki <piyush.karki@lmu.de>
2025-07-07 19:04:28 +0200

    added data generation for convergence test

--- scripts/generate_test_outputs.sh
+++ scripts/generate_test_outputs.sh
@@ -19,14 +19,19 @@ cd $REFERENCE_CONFIGS
 if [[ "$DIM" == "2" ]]; then
   gmsh -2 circular_hole.geo
   ${EXECUTABLE_DIR}/static circular_hole.toml \
-    --resolution 0.8 --matrix_free yes --mg_strategy twolevel \
+    -matrix_free yes --mg_strategy twolevel \
     --mg_coarse_level 1 --output ${TEMP_TEST_RESULTS}/output2D \
     --petsc -options_file mg_cheby.cfg
   for i in 1 2 4 8; do
     mpirun --oversubscribe -n $i ${EXECUTABLE_DIR}/static circular_hole.toml \
-      --resolution 0.8 --matrix_free yes --mg_strategy twolevel \
+      --matrix_free yes --mg_strategy twolevel \
       --mg_coarse_level 1 --output ${TEMP_TEST_RESULTS}/parallel_output2D_$i \
       --petsc -options_file mg_cheby.cfg
+  done
+  for h in 0.017 0.034 0.074 0.150 0.380 0.7845; do
+    gmsh -2 circular_hole.geo -setnumber h $h
+    ${EXECUTABLE_DIR}/static circular_hole.toml \
+      --petsc -options_file mg_cheby.cfg >>${TEMP_TEST_RESULTS}/convergence_2D.log
   done
   gmsh -2 bp1_ref.geo
   ${EXECUTABLE_DIR}/tandem bp1_ref_QD.toml \
@@ -38,14 +43,19 @@ if [[ "$DIM" == "2" ]]; then
 elif [[ "$DIM" == "3" ]]; then
   gmsh -3 spherical_hole.geo
   ${EXECUTABLE_DIR}/static spherical_hole.toml \
-    --resolution 0.8 --matrix_free yes --mg_strategy twolevel \
+    -matrix_free yes --mg_strategy twolevel \
     --mg_coarse_level 1 --output ${TEMP_TEST_RESULTS}/output3D \
     --petsc -options_file mg_cheby.cfg
   for i in 1 2 4 8; do
     mpirun --oversubscribe -n $i ${EXECUTABLE_DIR}/static spherical_hole.toml \
-      --resolution 0.8 --matrix_free yes --mg_strategy twolevel \
+      -matrix_free yes --mg_strategy twolevel \
       --mg_coarse_level 1 --output ${TEMP_TEST_RESULTS}/parallel_output3D_$i \
       --petsc -options_file mg_cheby.cfg
+  done
+  for h in 0.074 0.147 0.289 0.540 0.712; do
+    gmsh -3 spherical_hole.geo -setnumber h $h
+    mpirun -np 100 ${EXECUTABLE_DIR}/static spherical_hole.toml \
+      --petsc -options_file mg_cheby.cfg >>${TEMP_TEST_RESULTS}/convergence_3D.log
   done
   rm spherical_hole.msh
 else
